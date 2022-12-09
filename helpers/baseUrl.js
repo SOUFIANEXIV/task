b103110e5141d8a81e04dd1853a4544d8c51d5ae
SOUFIANE CH <98940737+SOUFIANEXIV@users.noreply.github.com>
2022-12-09 23:42:39 +0100

    Update baseUrl.js

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl= process.env.NODE_ENV === 'production' ?   "https://task-five-eta.vercel.app/": "http://localhost:3000";
+const baseUrl= process.env.NODE_ENV === 'production' ?   "https://task-soufiane.vercel.app/": "http://localhost:3000";
 export default baseUrl
 
